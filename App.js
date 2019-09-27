761a1e4083ad7e86bebe51b833bc36abbb4f924e
Igor <igor@MacBook-Pro-Igor.local>
2019-09-27 17:56:57 +0300

    function restyling

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { Provider } from 'react-redux';
 import Calculator from './containers/Calculator';
 import store from './store';
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <Calculator />
-    </Provider>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Calculator />
+  </Provider>
+);
+
+export default App;
